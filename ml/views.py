b39d1c7ab83fcc66bc22e6fcd3026c4c6eb0e5bc
ramuuzz <ramts3012005@gmail.com>
2025-06-29 14:57:14 +0530

    done

--- ml/views.py
+++ ml/views.py
@@ -12,7 +12,7 @@ def forminfo(request):
         Friends_circle_size = float(request.POST.get('Friends_circle_size', 0.0))
         Post_frequency = float(request.POST.get('Post_frequency', 0.0))
         Stage_fear = 1 if Stage_fear>5 else 0
-        Going_outside = 1 if Going_outside>5 else 0
+        Drained_after_socializing = 1 if Drained_after_socializing>5 else 0
 
         y_pred = model.predict([[Time_spent_Alone, Stage_fear, Social_event_attendance, Going_outside, Drained_after_socializing, Friends_circle_size, Post_frequency]])
         if y_pred[0] == 0:
